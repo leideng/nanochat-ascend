bcd95b5e9af0cdd385c53cf894454b086dab93de
Lei Deng <ldeng.sjtu@gmail.com>
2026-02-20 00:13:06 +0800

    eval and save checkpoints every 100 steps

--- runs/speedrun.sh
+++ runs/speedrun.sh
@@ -67,7 +67,7 @@ python -m scripts.tok_eval
 # Base model (pretraining)
 
 # d24 model (slightly overtrained is enough to beat GPT-2 => increase data:params ratio from compute optimal 10.5 (default) to 12)
-torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.base_train -- --depth=26 --target-param-data-ratio=8.25 --device-batch-size=8 --run=$WANDB_RUN
+torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.base_train -- --depth=26 --target-param-data-ratio=8.25 --device-batch-size=8 --eval-every=100 --save-every=100 --run=$WANDB_RUN
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
 torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.base_eval -- --device-batch-size=16
 
