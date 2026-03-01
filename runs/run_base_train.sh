9679971f108faa293fddda3e4834e0c96d7eaf5e
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-01 21:25:11 +0800

    Update run_base_train.sh to dynamically set the number of processes based on available CPU cores and add a CPU count echo statement for clarity.

--- runs/run_base_train.sh
+++ runs/run_base_train.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
+NUM_CPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
+echo "NUM_CPU in this machine is: $NUM_CPU"
+
+
 # d24 model (slightly overtrained is enough to beat GPT-2 => increase data:params ratio from compute optimal 10.5 (default) to 12)
-torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.base_train -- \
+torchrun --standalone --nproc_per_node=$NUM_CPU --master-addr=127.0.0.1 -m scripts.base_train -- \
 --depth=26 \
 --target-param-data-ratio=8.25 \
 --device-batch-size=8 \
 --eval-every=100 \
 --save-every=100 \
---run=$WANDB_RUN
+--run=$WANDB_RUN \
