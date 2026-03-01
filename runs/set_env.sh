f9adacf0b82720948001f17dcd66d440297f2514
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-01 21:15:45 +0800

    print usage if it is run in direct shell mode (not sourced mode)

--- runs/set_env.sh
+++ runs/set_env.sh
@@ -4,15 +4,17 @@
 # Usage:
 # source runs/set_env.sh
 #
-# This script is used to set the NANOCHAT_CONFIG environment variable
-# and print the value of the environment variable
+# This script is used to set the global environment variable
 #
 
+if [[ -n "${BASH_SOURCE[0]:-}" && "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "Usage: source runs/set_env.sh"
+  echo "Warning: do not execute this script (i.e. bash runs/set_env.sh), but source it"
+  exit 1
+fi
 
-echo "Usage: source runs/set_env.sh"
-echo "Warning: do not execute this script (i.e. bash runs/set_env.sh), but source it"
-
+export OMP_NUM_THREADS=1
 export NANOCHAT_CONFIG="configs/local.yaml"
 
-echo "NANOCHAT_CONFIG: $NANOCHAT_CONFIG"
+echo "NANOCHAT_CONFI is set to be: $NANOCHAT_CONFIG"
 echo "Please run \"echo \$NANOCHAT_CONFIG\" in your terminal to verify the environment variable is set"
