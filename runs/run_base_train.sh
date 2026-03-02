e61009679bfc2f42ae6384f2f6dfa719a5fc0383
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-02 22:28:53 +0800

    forgot to setup WANDB_RUN env

--- runs/run_base_train.sh
+++ runs/run_base_train.sh
@@ -3,6 +3,7 @@
 NUM_CPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
 echo "NUM_CPU in this machine is: $NUM_CPU"
 
+WANDB_RUN=dummy
 
 # d24 model (slightly overtrained is enough to beat GPT-2 => increase data:params ratio from compute optimal 10.5 (default) to 12)
 torchrun --standalone --nproc_per_node=$NUM_CPU --master-addr=127.0.0.1 -m scripts.base_train -- \
