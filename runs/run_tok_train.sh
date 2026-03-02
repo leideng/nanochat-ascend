ed35e1560dc4f955d306547e217bb1f406bb0f6e
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-02 22:13:21 +0800

    update tok train script

--- runs/run_tok_train.sh
+++ runs/run_tok_train.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# train the tokenizer
+echo "Training the tokenizer..."
 python -m scripts.tok_train --max-chars=2000000
+
+# evaluate the tokenizer (report compression ratio etc.)
+echo "Evaluating the tokenizer..."
 python -m scripts.tok_eval
