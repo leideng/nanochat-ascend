3b6478af7836e81599cd240093ce7f9595c0913e
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-15 20:58:57 +0800

    Remove synthetic identity conversations download from speedrun.sh; dataset has been prepared well

--- runs/speedrun.sh
+++ runs/speedrun.sh
@@ -65,11 +65,6 @@ torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.bas
 
 # -----------------------------------------------------------------------------
 # SFT (teach the model conversation special tokens, tool use, multiple choice)
-
-# download 2.3MB of synthetic identity conversations to impart a personality to nanochat
-# see dev/gen_synthetic_data.py for details on how this data was prepared and to get a sense of how you can easily tune it
-curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
-
 # run SFT and eval the model
 torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.chat_sft -- --device-batch-size=16 --run=$WANDB_RUN
 torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.chat_eval -- -i sft
