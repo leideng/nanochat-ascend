0aafec81790be3a6d3f64a3a6862cfb951c49375
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-15 20:56:04 +0800

    simply assume uv has been installed

--- runs/runcpu.sh
+++ runs/runcpu.sh
@@ -15,10 +15,9 @@ set -e
 
 source runs/set_env.sh
 
-command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
-[ -d ".venv" ] || uv venv
 uv sync --extra cpu
 source .venv/bin/activate
+
 if [ -z "$WANDB_RUN" ]; then
     WANDB_RUN=dummy
 fi
