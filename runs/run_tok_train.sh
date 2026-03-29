e0f97effe9e3b5ec484b6425391058b4ab7208cd
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-29 16:27:53 +0800

    train full tokenizer now

--- runs/run_tok_train.sh
+++ runs/run_tok_train.sh
@@ -9,7 +9,13 @@ source .venv/bin/activate
 
 # train the tokenizer
 echo "Training the tokenizer..."
-python -m scripts.tok_train --max-chars=5000000
+
+# for cpu only
+# python -m scripts.tok_train --max-chars=5000000
+python -m scripts.tok_train \
+  --max-chars=20_000_000_000 \
+  --doc-cap=10_000 \
+  --vocab-size=32768 
 
 # evaluate the tokenizer (report compression ratio etc.)
 echo "Evaluating the tokenizer..."
