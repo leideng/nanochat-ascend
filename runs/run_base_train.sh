ac36798fc18b56ae7ad78bc36a5383bdf2e6d81c
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-29 18:54:03 +0800

    add npu/cpu tranining branch

--- runs/run_base_train.sh
+++ runs/run_base_train.sh
@@ -12,12 +12,15 @@ source .venv/bin/activate
 PARALLEL_TRAIN="${PARALLEL_TRAIN:-0}"
 
 WANDB_RUN=dummy
+DEVICE_TYPE="cpu"
+if command -v npu-smi >/dev/null 2>&1 && npu-smi info >/dev/null 2>&1; then
+    DEVICE_TYPE="npu"
+fi
+echo "Detected device type: $DEVICE_TYPE"
 
-if [ $PARALLEL_TRAIN -eq 1 ]; then # (TODO) parallel training still not working. comming later
-    NUM_CPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
-    echo "NUM_CPU in this machine is: $NUM_CPU"
-    #echo "Using half of the CPUs for training: $((NUM_CPU / 2))"
-    .venv/bin/torchrun --standalone --nproc_per_node=$NUM_CPU --master-addr=127.0.0.1 -m scripts.base_train -- \
+if [ $DEVICE_TYPE == "npu" ]; then
+    echo "Training on NPU"
+    torchrun --standalone --nproc_per_node=1 -m scripts.base_train -- \
         --depth=4 \
         --head-dim=16 \
         --window-pattern=L \
@@ -32,21 +35,42 @@ if [ $PARALLEL_TRAIN -eq 1 ]; then # (TODO) parallel training still not working.
         --num-iterations=20 \
         --run=$WANDB_RUN \
         --model-tag="d4-test"
-else # single CPU training
-    echo "Running single-process CPU training smoke test. Set PARALLEL_TRAIN=1 to use torchrun."
-    python -m scripts.base_train \
-        --depth=4 \
-        --head-dim=16 \
-        --window-pattern=L \
-        --max-seq-len=128 \
-        --device-batch-size=8 \
-        --total-batch-size=1024 \
-        --eval-every=10 \
-        --eval-tokens=1024 \
-        --core-metric-every=-1 \
-        --sample-every=10 \
-        --save-every=10 \
-        --num-iterations=20 \
-        --run=$WANDB_RUN \
-        --model-tag="d4-test"
+else
+    if [ $PARALLEL_TRAIN -eq 1 ]; then # (TODO) parallel training still not working. comming later
+        NUM_CPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
+        echo "$DEVICE_TYPE: NUM_CPU in this machine is: $NUM_CPU"
+        #echo "Using half of the CPUs for training: $((NUM_CPU / 2))"
+        torchrun --standalone --nproc_per_node=$NUM_CPU --master-addr=127.0.0.1 -m scripts.base_train -- \
+            --depth=4 \
+            --head-dim=16 \
+            --window-pattern=L \
+            --max-seq-len=128 \
+            --device-batch-size=8 \
+            --total-batch-size=16384 \
+            --eval-every=10 \
+            --eval-tokens=1024 \
+            --core-metric-every=-1 \
+            --sample-every=10 \
+            --save-every=10 \
+            --num-iterations=20 \
+            --run=$WANDB_RUN \
+            --model-tag="d4-test"
+    else # single CPU training
+        echo "Running single-process training smoke test on $DEVICE_TYPE. Set PARALLEL_TRAIN=1 to use torchrun."
+        python -m scripts.base_train \
+            --depth=4 \
+            --head-dim=16 \
+            --window-pattern=L \
+            --max-seq-len=128 \
+            --device-batch-size=8 \
+            --total-batch-size=1024 \
+            --eval-every=10 \
+            --eval-tokens=1024 \
+            --core-metric-every=-1 \
+            --sample-every=10 \
+            --save-every=10 \
+            --num-iterations=20 \
+            --run=$WANDB_RUN \
+            --model-tag="d4-test"
+    fi
 fi
