fe2d15d5b6cbf72ff4ffbd430c67415118b48667
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-29 19:04:34 +0800

    update

--- runs/run_base_train.sh
+++ runs/run_base_train.sh
@@ -10,6 +10,9 @@ source runs/set_env.sh
 source .venv/bin/activate
 
 PARALLEL_TRAIN="${PARALLEL_TRAIN:-0}"
+MASTER_ADDR="${MASTER_ADDR:-127.0.0.1}"
+MASTER_PORT="${MASTER_PORT:-29500}"
+LOCAL_ADDR="${LOCAL_ADDR:-127.0.0.1}"
 
 WANDB_RUN=dummy
 DEVICE_TYPE="cpu"
@@ -18,9 +21,9 @@ if command -v npu-smi >/dev/null 2>&1 && npu-smi info >/dev/null 2>&1; then
 fi
 echo "Detected device type: $DEVICE_TYPE"
 
-if [ $DEVICE_TYPE == "npu" ]; then
+if [ "$DEVICE_TYPE" == "npu" ]; then
     echo "Training on NPU"
-    torchrun --standalone --nproc_per_node=16 --master-addr=127.0.0.1 -m scripts.base_train -- \
+    torchrun --nproc_per_node=16 --master-addr="$MASTER_ADDR" --master-port="$MASTER_PORT" --local-addr="$LOCAL_ADDR" -m scripts.base_train -- \
         --depth=4 \
         --head-dim=16 \
         --window-pattern=L \
@@ -36,11 +39,11 @@ if [ $DEVICE_TYPE == "npu" ]; then
         --run=$WANDB_RUN \
         --model-tag="d4-test"
 else
-    if [ $PARALLEL_TRAIN -eq 1 ]; then # (TODO) parallel training still not working. comming later
+    if [ "$PARALLEL_TRAIN" -eq 1 ]; then # (TODO) parallel training still not working. comming later
         NUM_CPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
         echo "$DEVICE_TYPE: NUM_CPU in this machine is: $NUM_CPU"
         #echo "Using half of the CPUs for training: $((NUM_CPU / 2))"
-        torchrun --standalone --nproc_per_node=$NUM_CPU --master-addr=127.0.0.1 -m scripts.base_train -- \
+        torchrun --nproc_per_node="$NUM_CPU" --master-addr="$MASTER_ADDR" --master-port="$MASTER_PORT" --local-addr="$LOCAL_ADDR" -m scripts.base_train -- \
             --depth=4 \
             --head-dim=16 \
             --window-pattern=L \
