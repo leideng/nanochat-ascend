04559a1905fca90370cb69d842ba881f22fd84c9
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-29 15:57:24 +0800

    works now

--- rsync-dataset-to-a3.sh
+++ rsync-dataset-to-a3.sh
@@ -2,22 +2,21 @@
 set -euo pipefail
 
 # Usage:
-#   bash rsync-a3.sh
+#   bash rsync-dataset-to-a3.sh
 
 
-DEST="root@a3:/data/ldeng/code/nanochat-ascend"
+DEST_BASE="root@a3:/data/ldeng/code/nanochat-ascend/.cache/dataset"
 
-echo "Start to rsync dataset in .cache to A3 at $DEST"
+echo "Start rsync dataset to A3: $DEST_BASE"
 
 # Run from repository root so relative paths always resolve.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
-# do not rsycn pretrain dataset;
-# manually set in a3 server
+# Do not sync pretrain dataset; set that manually on A3.
 rsync -avz --info=progress2 --human-readable \
   ".cache/dataset/eval" \
   ".cache/dataset/task" \
-  "$DEST"
+  "$DEST_BASE/"
 
-echo "Successfully rsync dataset in .cache to A3 at $DEST"
+echo "Successfully synced dataset in .cache to A3: $DEST_BASE"
