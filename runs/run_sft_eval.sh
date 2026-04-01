6810dea00d7d6af14e329f312a0aa9c82d87be4d
Lei Deng <ldeng.sjtu@gmail.com>
2026-04-01 23:09:06 +0800

    add sft eval script

---
+++ runs/run_sft_eval.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+
+[ -d ".venv" ] || uv venv
+source .venv/bin/activate
+
+source runs/set_env.sh
+
+MASTER_ADDR="${MASTER_ADDR:-127.0.0.1}"
+MASTER_PORT="${MASTER_PORT:-29500}"
+LOCAL_ADDR="${LOCAL_ADDR:-127.0.0.1}"
+
+# Evaluate the model
+torchrun --nproc_per_node=16 --master-addr="$MASTER_ADDR" --master-port="$MASTER_PORT" --local-addr="$LOCAL_ADDR" -m scripts.chat_eval -- \
+    --source="sft" \
+    --model-tag="d20" \
+    --batch-size=48 \
+    --eval=core,bpb,sample
