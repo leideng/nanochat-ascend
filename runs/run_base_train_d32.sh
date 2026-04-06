b9d459fa93587a1f8e03f5cdc16e8f8a997def5d
Lei Deng <ldeng.sjtu@gmail.com>
2026-04-06 23:34:00 +0800

    train another 1500 steps

--- runs/run_base_train_d32.sh
+++ runs/run_base_train_d32.sh
@@ -35,8 +35,8 @@ if [ "$DEVICE_TYPE" == "npu" ]; then
         --device-batch-size=4 \
         --total-batch-size=-1 \
         --target-param-data-ratio=20 \
-        --num-iterations=9500 \
-        --resume-from-step=8000 \
+        --num-iterations=11000 \
+        --resume-from-step=9500 \
         --eval-every=200 \
         --core-metric-every=200 \
         --sample-every=200 \
