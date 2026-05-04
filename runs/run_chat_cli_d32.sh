ee840874991bafe039f0984aaf1e34bb348f4a7e
Lei Deng <ldeng.sjtu@gmail.com>
2026-05-05 00:53:23 +0800

    add run d32 seperate scipt

---
+++ runs/run_chat_cli_d32.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# exit on error
+set -e
+
+source runs/set_env.sh
+
+# create a .venv local virtual environment (if it doesn't exist)
+[ -d ".venv" ] || uv venv
+source .venv/bin/activate
+
+
+DEVICE_TYPE="cpu"
+if command -v npu-smi >/dev/null 2>&1 && npu-smi info >/dev/null 2>&1; then
+    DEVICE_TYPE="npu"
+fi
+echo "Detected device type: $DEVICE_TYPE"
+
+
+python -m scripts.chat_cli \
+    --model-tag="d32" \
+    --source="rl" \
+    --device-type=$DEVICE_TYPE
